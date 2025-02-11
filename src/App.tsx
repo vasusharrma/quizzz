81805fdbc1589bf83d44394bbe2491117e7744b7
vasusharrma <sharmavashu099@gmail.com>
2025-02-11 15:42:03 +0530

    ts FC<props>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import Button from './componenets/Button'
+
 function App() {
   return (
-    <>
+    <div>
       <h1 className='bg-blue-700 text-white text-center'>Hello</h1>
-    </>
+      <Button background_color={'bg-blue-800'} fontSize={30} />
+    </div>
   )
 }
 
---
+++ src/context/ProductProvider.tsx
@@ -0,0 +1,5 @@
+export type ProductType = {
+  sku: string
+  name: string
+  price: number
+}
